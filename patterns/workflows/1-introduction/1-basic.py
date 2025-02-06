2078d32a4d90fa1636f6c3736fff05e733e1353a
Hassen Taidirt <hi@hassen.io>
2025-02-06 11:50:14 +0400

    Edit first example to properly run locally

--- patterns/workflows/1-introduction/1-basic.py
+++ patterns/workflows/1-introduction/1-basic.py
@@ -1,12 +1,15 @@
 import os
 
+from dotenv import load_dotenv
 from openai import OpenAI
+
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 completion = client.chat.completions.create(
-    model="gpt-4o",
+    model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You're a helpful assistant."},
         {
